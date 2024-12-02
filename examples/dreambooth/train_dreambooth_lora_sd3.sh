3b1cb1949a4c34670eef95f321ae20d9fb4d0b5f
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2024-12-02 23:01:07 +0200

    Add script for DreamBooth LoRa SD3 training

---
+++ examples/dreambooth/train_dreambooth_lora_sd3.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$MODEL_NAME" ] || [ -z "$OUTPUT_DIR" ] || [ -z "$INSTANCE_DATA_DIR" ]  || [ -z "$INSTANCE_PROMPT" ] || [ -z "$CLASS_DIR" ] || [ -z "$CLASS_PROMPT" ] || [ -z "$VALIDATION_PROMPT" ]; then
+  echo Please define the following environment variables correctly:
+  echo MODEL_NAME="$MODEL_NAME"
+  echo OUTPUT_DIR="$OUTPUT_DIR"
+  echo INSTANCE_DATA_DIR="$INSTANCE_DATA_DIR"
+  echo INSTANCE_PROMPT="$INSTANCE_PROMPT"
+  echo CLASS_DIR="$CLASS_DIR"
+  echo CLASS_PROMPT="$CLASS_PROMPT"
+  echo VALIDATION_PROMPT="$VALIDATION_PROMPT"
+  exit 1
+fi
+
+accelerate launch train_dreambooth_lora_sd3.py \
+  --pretrained_model_name_or_path="$MODEL_NAME"  \
+  --instance_data_dir="$INSTANCE_DATA_DIR" \
+  --output_dir="$OUTPUT_DIR-$(date +"%Y%m%d_%H%M%S")" \
+  --instance_prompt="$INSTANCE_PROMPT" \
+  --resolution=512 \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 \
+  --checkpointing_steps=100 \
+  --learning_rate=4e-4 \
+  --report_to="wandb" \
+  --lr_scheduler="constant" \
+  --lr_warmup_steps=0 \
+  --max_train_steps=500 \
+  --validation_prompt="$VALIDATION_PROMPT" \
+  --validation_epochs=25 \
+  --seed="0" \
+  --mixed_precision="fp16" \
+  --use_8bit_adam \
+  --push_to_hub
+
+#  --with_prior_preservation \
+#  --class_data_dir="$CLASS_DIR" \
+#  --class_prompt="$CLASS_PROMPT"\
+#  --num_class_images=200 \
+#  --optimizer="AdamW" \
+#  --use_8bit_adam \
