92d4f923a0ddc0e836c7bacfe0fc73512bc9a0aa
Mikko Reinikainen <mikko.reinikainen@reaktor.com>
2024-12-02 21:48:15 +0200

    Adjust training parameters

--- examples/dreambooth/train_dreambooth_sd3.sh
+++ examples/dreambooth/train_dreambooth_sd3.sh
@@ -6,16 +6,15 @@ if [ -z "$MODEL_NAME" ] || [ ! -d "$OUTPUT_DIR" ] || [ -z "$INSTANCE_DATA_DIR" ]
   exit 1
 fi
 
-
 accelerate launch train_dreambooth_sd3.py \
   --pretrained_model_name_or_path="$MODEL_NAME"  \
   --instance_data_dir="$INSTANCE_DATA_DIR" \
-  --output_dir="$OUTPUT_DIR" \
-  --mixed_precision="fp16" \
+  --output_dir="$OUTPUT_DIR-$(date +"%Y%m%d_%H%M%S")" \
   --instance_prompt="$INSTANCE_PROMPT" \
   --resolution=512 \
   --train_batch_size=1 \
-  --gradient_accumulation_steps=4 \
+  --gradient_accumulation_steps=1 \
+  --checkpointing_steps=100 \
   --learning_rate=5e-5 \
   --report_to="wandb" \
   --lr_scheduler="constant" \
@@ -27,7 +26,7 @@ accelerate launch train_dreambooth_sd3.py \
   --class_data_dir="$CLASS_DIR" \
   --class_prompt="$CLASS_PROMPT"\
   --num_class_images=200 \
-
+  --mixed_precision="fp16" \
   --push_to_hub
 
 #  --optimizer="AdamW" \
